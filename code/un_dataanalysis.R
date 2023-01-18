c4c37fd963635fe3116bcdcb2dc791f1f70ae86c
danieltetreault7289 <danieltetreault7289@gmail.com>
2023-01-18 14:05:16 -0500

    day 2 afternoon lesson 2

--- code/un_dataanalysis.R
+++ code/un_dataanalysis.R
@@ -62,6 +62,45 @@ anti_join(co2_emissions, gapminder_data_2007, by = "country")
 
 full_join(co2_emissions, gapminder_data_2007) %>% View()
 
-write_csv(joined_co2_pop, file = "data/joined_c02_pop.csv")
+write_csv(joined_co2_pop, file = "data/joined_co2_pop.csv")
 
+joined_co2_pop <- read_csv("data/joined_co2_pop.csv")
 
+ggplot(data = joined_co2_pop) +
+  aes(x = country, y = gdpPercap) +
+  labs(x = "Country", y = "GDP per Capita") +
+  geom_boxplot()
+
+ggplot(data = joined_co2_pop) +
+  aes(x = lifeExp) +
+  labs(x = "Life Expectancy (Years)", y = "Frequency") +
+  geom_histogram(bins = 50, fill = "cornflowerblue") +
+  ##scale_color_brewer(palette = "Set2") +
+  theme_prism() 
+
+install.packages("ggthemes")
+library("ggthemes")
+install.packages("ggprism")
+library("ggprism")
+
+ggplot(data = joined_co2_pop) +
+  aes(x = gdpPercap) +
+  labs(x = "GDP per Capita (USD)", y = "Frequency") +
+  geom_histogram(bins = 50, fill = "pink") +
+  theme_prism()
+
+gdp_co2_plot <- joined_co2_pop %>% 
+  ggplot(aes(x = gdpPercap, y = per_capita_emissions)) +
+  labs (x = "GDP per Capita (USD)", y = "Per Capita CO2 Emissions (metric tons)", title = "It's possible to have high GDP and low CO2 emissions!") +
+  geom_point(color = "cornflowerblue") +
+  geom_smooth(color = "pink", method = "lm") + ##se = FALSE to get rid of gray shadow
+  labs(caption = "Pink line represents standard linear regression model.") +
+  theme_classic() +
+  ggpubr::stat_regline_equation(aes(label = after_stat(rr.label)))+
+  theme(plot.background = element_rect(fill = "pink"))
+
+install.packages("ggpubr")
+library(ggpubr)
+
+ggsave(gdp_co2_plot, filename = "figures/gdp_vs_co2_plot.png", 
+       height = 4, width = 6, units = "in", dpi = 300)
